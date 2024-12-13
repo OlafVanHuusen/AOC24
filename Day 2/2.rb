6e0cff1d4d163015f6ea4e8a84c21e14f87b30b8
OlafVanHuusen <lennard.clicque@fau.de>
2024-12-13 22:31:49 +0100

    incredibly ugly Day 2 solution

--- Day 2/2.rb
+++ Day 2/2.rb
@@ -1,5 +1,13 @@
 def main
-
+  reports = File.readlines('Day 2/input.txt').map { |numberString|  numberString.split(' ').map { |number| number.to_i} }
+  task1(reports)
+  task2(reports)
 end
 
-main
+def task1(reports)
+  puts reports.reduce(0) { |sum, report| sum + (report.each_cons(2).reduce([true, 0]) { |acc, (a, b)| [acc[0] && ((a - b).abs <= 3) && ((acc[1] == 0)  || ((acc[1] == 1) ? a < b : b < a)), (a < b ? 1 : 2)] }[0] ? 1 : 0) } - 1
+end
+
+def task2(reports)
+  puts reports.reduce(0) { |sum, report| sum + ((-1..(report.length - 1)).to_a.reduce(false) { |acc, i| acc || (i == -1 ? report.clone : report.clone.tap { |r| r.delete_at(i) }).each_cons(2).reduce([true, 0]) { |acc2, (a, b)| [acc2[0] && ((a - b).abs <= 3) && (((acc2[1] == 0) && (a != b)) || ((acc2[1] == 1) ? a < b : b < a)), (a < b ? 1 : 2)] }[0]} ? 1 : 0) }
+end
