140c4184ac94b3593b3648b993890bf902a5d410
OlafVanHuusen <lennard.clicque@fau.de>
2024-12-23 20:50:36 +0100

    updated file creation script

--- utils/createStuff.rb
+++ utils/createStuff.rb
@@ -8,7 +8,8 @@ def createAllDirectoriesAndFiles(directory)
   missingDays.each do |day|
     Dir.mkdir("Day #{day}") unless File.exist?("Day #{day}")
     File.new("Day #{day}/input.txt", "w") unless File.exist?("Day #{day}/input.txt")
-    File.new("Day #{day}/#{day}.rb", "w") unless File.exist?("Day #{day}/#{day}.rb")
+    #File.new("Day #{day}/#{day}.rb", "w") unless File.exist?("Day #{day}/#{day}.rb")
+    File.write("Day #{day}/#{day}.rb", "require 'aoc_utils'\n\n\ndef main\n\n\nend\n\n\nmain") unless File.exist?("Day #{day}/#{day}.rb")
     File.new("Day #{day}/testinput.txt", "w") unless File.exist?("Day #{day}/testinput.txt")
   end
 end
